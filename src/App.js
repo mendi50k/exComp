1bc4496d94d60793e05c76aa7028bedad3775c85
mendi50k <menachem3211231@gmail.com>
2024-12-12 00:53:46 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import Comp from "../src/templates/comp"
+function App() {
+  return (
+    <div className="App">
+      {/* <div className="test"></div>
+      <div className="test1">test1</div>
+      <div className="test2">test2</div>
+      <p>ptest1</p>
+      <p>ptest2</p>
+
+      <a href="#">testA1</a>
+      <a href="#">testA2</a>
+      <span>test</span> */}
+    <Comp />
+
+
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/templates/comp.js
@@ -0,0 +1,28 @@
+import "./comp.css";
+
+export default function Comp() {
+  return (
+    <div>
+      <header>
+        <h1>Book Component By Function</h1>
+      </header>
+
+      <main>
+        <div>
+          <p>mendi50k on tiktok is a legend</p>
+          <p>Scope and colosure</p>
+          <p>JS</p>
+        </div>
+
+        <h2>By Kyle Simpson</h2>
+        <p>
+          parts of the language that many JavaScript pogrammers simply avoid
+        </p>
+      </main>
+
+      <footer>
+        <p>&copy; 2022</p>
+      </footer>
+    </div>
+  );
+}
